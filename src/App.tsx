b68f81449953a565e4258cae701a4246e77119da
ruanvsrateira <vruan85@gmail.com>
2023-11-15 00:56:33 -0300

    feat: create form

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,56 @@
 import { useFormik } from "formik";
 import { useState } from "react";
-import { object, string } from "yup";
+import { bool, object, ref, string } from "yup";
+import { GoArrowLeft, GoArrowRight, GoCheck } from "react-icons/go";
+import { Input } from "./components/Input";
+import { Button } from "./Button";
+import { StepIndicator } from "./components/StepIndicator";
 
 function App() {
   const [currentStep, setCurrentStep] = useState<number>(0);
   const steps = [
     {
       step: 0,
-      title: "Email",
+      title: "Configuração de E-mail",
+      status: "not finalized",
       validationSchema: object({
-        email: string().required("campo obrigatorio"),
-        confirmEmail: string().required("campo obrigatório"),
+        email: string().email("E-mail Inválido").required("campo obrigatorio"),
+        confirmEmail: string()
+          .required("Campo obrigatório")
+          .oneOf([ref("email")], "E-mail não coincidem"),
       }),
     },
     {
       step: 1,
-      title: "Password",
+      title: "Configuração de senha",
+      status: "not finalized",
       validationSchema: object({
-        password: string().min(4).required("campo obrigatorio"),
+        password: string()
+          .min(4, "A senha deve ter mais de 4 caractéres")
+          .required("Campo obrigatório"),
+        confirmPassword: string()
+          .required("Campo obrigatório")
+          .oneOf([ref("password")], "As Senhas não se coincidem"),
+      }),
+    },
+    {
+      step: 2,
+      title: "Termos de Privacidade",
+      status: "not finalized",
+      validationSchema: object({
+        acceptTerms: bool().oneOf(
+          [true],
+          "Para finalizar você deve aceitar os termos"
+        ),
       }),
     },
   ];
 
   function handlePreviousStep() {
-    if (currentStep == 0) {
-      setCurrentStep(steps.length - 1);
-    }
-
     setCurrentStep((p) => p - 1);
   }
 
   function handleNextStep() {
-    if (currentStep == steps.length - 1) {
-      setCurrentStep(0);
-    }
-
     setCurrentStep((p) => p + 1);
   }
 
@@ -43,6 +59,8 @@ function App() {
       email: "",
       password: "",
       confirmEmail: "",
+      confirmPassword: "",
+      acceptTerms: true,
     },
     validationSchema: steps[currentStep].validationSchema,
     onSubmit: (values) => {
@@ -50,55 +68,106 @@ function App() {
         alert(JSON.stringify(values));
         return;
       }
-
       return handleNextStep();
     },
     validateOnChange: true,
   });
 
   return (
-    <main className="bg-red-200">
-      <form onSubmit={formik.handleSubmit}>
-        <div>
-          {currentStep == 0 ? (
-            <>
-              <input
-                name="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                placeholder="Email"
-              />
-              <input
-                name="confirmEmail"
-                value={formik.values.confirmEmail}
-                onChange={formik.handleChange}
-                placeholder="Confirme o E-mail"
-              />
-            </>
-          ) : null}
-          {currentStep == 1 ? (
-            <input
-              name="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              placeholder="Password"
+    <main className="bg-purple-400 w-[100vw] h-[100vh] flex items-center justify-center">
+      <div className="min-w-[600px] p-10 bg-white rounded-md shadow-md">
+        <div className="mb-10 flex justify-around gap-6">
+          {steps.map((s) => (
+            <StepIndicator
+              status={s.status}
+              currentStep={currentStep}
+              step={s.step}
+              title={s.title}
             />
-          ) : null}
-
-          {currentStep > 0 ? (
-            <button onClick={handlePreviousStep}>voltar</button>
-          ) : null}
-          {currentStep !== steps.length - 1 ? (
-            <button type="submit" disabled={!formik.isValid}>
-              Próximo
-            </button>
-          ) : (
-            <button type="submit" disabled={!formik.isValid}>
-              Finalizar Formulário
-            </button>
-          )}
+          ))}
         </div>
-      </form>
+        <form onSubmit={formik.handleSubmit}>
+          <div className="flex flex-col items-center gap-2">
+            {currentStep == 0 ? (
+              <>
+                <Input
+                  type="input"
+                  name="email"
+                  error={formik.errors.email}
+                  onChange={formik.handleChange}
+                  placeholder="Digite seu melhor e-mail"
+                  value={formik.values.email}
+                />
+                <Input
+                  type="input"
+                  name="confirmEmail"
+                  error={formik.errors.confirmEmail}
+                  onChange={formik.handleChange}
+                  placeholder="Confirme novamente seu e-mail"
+                  value={formik.values.confirmEmail}
+                />
+              </>
+            ) : null}
+            {currentStep == 1 ? (
+              <>
+                <Input
+                  type="input"
+                  name="password"
+                  error={formik.errors.password}
+                  onChange={formik.handleChange}
+                  placeholder="Digite uma boa senha"
+                  value={formik.values.password}
+                />
+                <Input
+                  type="input"
+                  name="confirmPassword"
+                  error={formik.errors.confirmPassword}
+                  onChange={formik.handleChange}
+                  placeholder="Confirme novamente sua senha"
+                  value={formik.values.confirmPassword}
+                />
+              </>
+            ) : null}
+            {currentStep == 2 ? (
+              <Input
+                type="checkbox"
+                name="acceptTerms"
+                error={formik.errors.acceptTerms}
+                onChange={formik.handleChange}
+                placeholder="Eu confirmo que li e aceito os termos"
+                value={formik.values.confirmPassword}
+              />
+            ) : null}
+            <div className="w-full flex flex-row gap-4 items-center">
+              {currentStep > 0 ? (
+                <Button
+                  text="Voltar"
+                  leftIcon={<GoArrowLeft size={22} />}
+                  variant="purple"
+                  type="button"
+                  onClick={handlePreviousStep}
+                />
+              ) : null}
+
+              {currentStep !== steps.length - 1 ? (
+                <Button
+                  text="Próximo"
+                  rightIcon={<GoArrowRight size={22} />}
+                  variant="purple"
+                  type="submit"
+                />
+              ) : (
+                <Button
+                  text="Finalizar Cadastro"
+                  rightIcon={<GoCheck size={22} />}
+                  variant="green"
+                  type="submit"
+                />
+              )}
+            </div>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
