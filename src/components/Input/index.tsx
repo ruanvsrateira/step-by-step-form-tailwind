d7f6942be5180fc69cf566346d68bacfd0e535fd
ruanvsrateira <vruan85@gmail.com>
2023-11-15 00:56:15 -0300

    feat: add variant checkbox

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,9 +5,22 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   value: string;
   error: string | undefined;
   placeholder: string;
+  type: "input" | "checkbox";
 }
 
 export const Input = ({ error, ...props }: InputProps) => {
+  if (props.type == "checkbox") {
+    return (
+      <div className="mb-4">
+        <div className="flex gap-2">
+          <input {...props} />
+          {props.placeholder}
+        </div>
+        {error && <span className="text-gray-600 italic">({error})</span>}
+      </div>
+    );
+  }
+
   return (
     <div className="w-full">
       <input
